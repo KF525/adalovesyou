2d5d8c8215123d7f2ef904e6fc4765db93705481
Kate Fulton <katesuzannefulton@gmail.com>
2015-04-15 16:31:37 -0700

    work on statuses that mention bot

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,46 @@
+import sun.security.krb5.Credentials
 import twitter4j.auth.AccessToken
 import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
 
 object Main extends App {
+
   val twitter: Twitter = new TwitterFactory().getInstance()
   twitter.setOAuthConsumer(sys.env("CONSUMER_KEY"),
     sys.env("CONSUMER_SECRET"))
   twitter.setOAuthAccessToken(new AccessToken(sys.env("ACCESS_TOKEN"),
     sys.env("ACCESS_TOKEN_SECRET")))
-  private val timeline: ResponseList[Status] = twitter.getHomeTimeline()
-  twitter.updateStatus("#NotABot")
+  val status: ResponseList[Status] = twitter.getMentionsTimeline();
+  //private val timeline: ResponseList[Status] = twitter.getHomeTimeline()
+  //twitter.updateStatus("#NotABot")
 
-  Console.println("Hello World: " + timeline.toString)
+  Console.println("Hello World: " + status.toString)
 
-  val twitterStreamFactory = new TwitterStreamFactory
-  val twitterStream = twitterStreamFactory.getInstance()
+  def listener = new StatusListener() {
+    def onStatus(status: Status) {println(status.getText)}
+    def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+    def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
+    def onException(ex: Exception) {ex.printStackTrace}
+    def onScrubGeo(arg0: Long, arg1: Long) {}
+    def onStallWarning(warning: StallWarning) {}
+  }
+
+  val twitterStream = new TwitterStreamFactory().getInstance()
   twitterStream.setOAuthConsumer(sys.env("CONSUMER_KEY"),
     sys.env("CONSUMER_SECRET"))
   twitterStream.setOAuthAccessToken(new AccessToken(sys.env("ACCESS_TOKEN"),
     sys.env("ACCESS_TOKEN_SECRET")))
-  twitterStream.sample("en")
+  twitterStream.addListener(listener)
+  twitterStream.user()
+  Thread.sleep(6000)
+  twitterStream.cleanUp
+  twitterStream.shutdown
+}
+
+
+
+
+
 
 
   //TO DO:
@@ -29,5 +51,7 @@ object Main extends App {
   //respond to users who use those specific phrases/hashtag
   //try to specify type of tweet to send out??
   //create a collection of tweets to select from and tweet to user
-}
+
+
+
 
