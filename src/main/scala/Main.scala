8dd4ffc67403d4989a591d15e70ecb2e600afe1a
Kate Fulton <katesuzannefulton@gmail.com>
2015-04-22 09:18:03 -0700

    Follow and unfollow appropriately

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,3 @@
-import sun.security.krb5.Credentials
-import twitter4j.auth.AccessToken
 import twitter4j._
 import twitter4j.conf.ConfigurationBuilder
 
@@ -13,53 +11,38 @@ object Main extends App {
     .build()
 
   val twitter = new TwitterFactory(config).getInstance
-  val stream = new TwitterStreamFactory(config).getInstance()
-
-//  private val timeline: ResponseList[Status] = twitter.getHomeTimeline
-//  private val myMentions = twitter.getMentionsTimeline
-//  twitter.updateStatus("Ada loves you!")
-//  Console.println("Hello World: " + timeline.toString + myMentions.toString )
+  val stream = new TwitterStreamFactory(config).getInstance
 
   val listener = new StatusListener() {
     def onStatus(status: Status) {println(status.getText)}
     def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
     def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
-    def onException(ex: Exception) {ex.printStackTrace}
+    def onException(ex: Exception) { ex.printStackTrace() }
     def onScrubGeo(arg0: Long, arg1: Long) {}
     def onStallWarning(warning: StallWarning) {}
   }
 
-  // gather an array of all followers' IDs of ScalaBot
-  //Console.println(twitter.getFollowersIDs(3153362684L, -1).toString())
-
-  // gather an array of all followers general info 
-  //Console.println(twitter.getFollowersList(3153362684L, -1).toString())
-
-
-
-  //go through friends and push things into toFollow/toUnfollow sequences appropriately
-  //sequences that are empty are not broken
-
   val getFollowers: IDs = twitter.getFollowersIDs(3153362684L, -1)
   val getFriends: IDs = twitter.getFriendsIDs(3153362684L, -1)
-  val uniqueIds: Set[Long] = getFollowers.getIDs.toSet.union(getFriends.getIDs.toSet)
+  val toFollowIDs: Set[Long] = getFollowers.getIDs.toSet.diff(getFriends.getIDs.toSet)
+  val toUnFollowIDs: Set[Long] = getFriends.getIDs.toSet.diff(getFollowers.getIDs.toSet)
 
-
-//  Console.println(twitter.getFollowersIDs(3153362684L, -1).toString())
-//  Console.println(twitter.getFriendsIDs(3153362684L, -1).toString())
-
-  val toFollow: Set[Long] = uniqueIds
+  val toFollow: Set[Long] = toFollowIDs
   toFollow.foreach { friend => twitter.createFriendship(friend) }
 
-  val toUnfollow: Set[Long] = Set()
+  val toUnfollow: Set[Long] = toUnFollowIDs
   toUnfollow.foreach { friend => twitter.destroyFriendship(friend) }
 
 //  stream.addListener(listener)
 //  stream.user()
 //  Thread.sleep(6000)
-//  stream.cleanUp
-//  stream.shutdown
+//  stream.cleanUp()
+//  stream.shutdown()
 
+  //  private val timeline: ResponseList[Status] = twitter.getHomeTimeline
+  //  private val myMentions = twitter.getMentionsTimeline
+  //  twitter.updateStatus("Ada loves you!")
+  //  Console.println("Hello World: " + timeline.toString + myMentions.toString )
 
 }
 
@@ -70,9 +53,6 @@ object Main extends App {
 
 
   //TO DO:
-  //create a twitter account
-  //get credentials and add to .env file
-  //get our list of followers
   //listen to twitter (all of twitter or just account's followers/or people who follow account?)
   //listen for specific phrases or hashtag (what should this/these be)
   //respond to users who use those specific phrases/hashtag
