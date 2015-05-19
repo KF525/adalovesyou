88d7304a241112c1cdb205151ba233534f1af43c
Rebecca Mark <rebecca.lmark@gmail.com>
2015-05-19 16:35:41 -0700

    started with userListener

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,7 +27,59 @@ object Main extends App {
     def onStallWarning(warning: StallWarning) {}
   }
 
-  println(twitterConfig.consumerKey)
+  val userStreamListener = new UserStreamListener {
+
+    override def onFriendList(friendIds: Array[Long]): Unit = ()
+
+    override def onUserListUnsubscription(subscriber: User, listOwner: User, list: UserList): Unit = ()
+
+    override def onBlock(source: User, blockedUser: User): Unit = ()
+
+    override def onUserListSubscription(subscriber: User, listOwner: User, list: UserList): Unit = ()
+
+    override def onFollow(source: User, followedUser: User): Unit = (println("I'm following a person"))
+
+    override def onUserListMemberAddition(addedMember: User, listOwner: User, list: UserList): Unit = ???
+
+    override def onDirectMessage(directMessage: DirectMessage): Unit = ???
+
+    override def onUnblock(source: User, unblockedUser: User): Unit = ???
+
+    override def onUserListUpdate(listOwner: User, list: UserList): Unit = ???
+
+    override def onUnfollow(source: User, unfollowedUser: User): Unit = ???
+
+    override def onUserProfileUpdate(updatedUser: User): Unit = ???
+
+    override def onUserListMemberDeletion(deletedMember: User, listOwner: User, list: UserList): Unit = ???
+
+    override def onUserDeletion(deletedUser: Long): Unit = ???
+
+    override def onDeletionNotice(directMessageId: Long, userId: Long): Unit = ???
+
+    override def onFavorite(source: User, target: User, favoritedStatus: Status): Unit = ???
+
+    override def onUnfavorite(source: User, target: User, unfavoritedStatus: Status): Unit = ???
+
+    override def onUserSuspension(suspendedUser: Long): Unit = ???
+
+    override def onUserListDeletion(listOwner: User, list: UserList): Unit = ???
+
+    override def onUserListCreation(listOwner: User, list: UserList): Unit = ???
+
+    override def onStallWarning(warning: StallWarning): Unit = ???
+
+    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = ???
+
+    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = ???
+
+    override def onStatus(status: Status): Unit = ???
+
+    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = ???
+
+    override def onException(ex: Exception): Unit = ()
+
+  }
 
 
 //  class NotificationListener(
@@ -77,8 +129,8 @@ object Main extends App {
   val toUnfollow: Set[Long] = toUnFollowIDs
   toUnfollow.foreach { friend => twitter.destroyFriendship(friend) }
 
-  stream.addListener(statusListener)
-  stream.user()
+  stream.addListener(userStreamListener.onFollow())
+  //stream.user()
   Thread.sleep(6000)
   stream.cleanUp()
   stream.shutdown()
@@ -97,6 +149,7 @@ object Main extends App {
 
 
   //TO DO:
+  //Create a usernotification listener which listens to new follow events.
   //listen to twitter (all of twitter or just account's followers/or people who follow account?)
   //listen for specific phrases or hashtag (what should this/these be)
   //respond to users who use those specific phrases/hashtag
