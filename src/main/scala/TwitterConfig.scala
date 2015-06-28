bdbad3f5d4e1c7e0b35a1c96267bba9ea4441533
Kate Fulton <katesuzannefulton@gmail.com>
2015-06-28 14:10:29 -0700

    update twitter config file to point to heroku-stored variables

--- src/main/scala/TwitterConfig.scala
+++ src/main/scala/TwitterConfig.scala
@@ -1,9 +1,9 @@
 import com.typesafe.config.Config
 
 class TwitterConfig(config: Config) {
-  val consumerKey = config.getString("consumerKey")
-  val consumerSecret = config.getString("consumerSecret")
-  val accessToken = config.getString("accessToken")
-  val accessTokenSecret = config.getString("accessTokenSecret")
+  val consumerKey = sys.env("CONSUMER_KEY") //config.getString("consumerKey")
+  val consumerSecret = sys.env("CONSUMER_SECRET") //config.getString("consumerSecret")
+  val accessToken = sys.env("ACCESS_TOKEN") //config.getString("accessToken")
+  val accessTokenSecret = sys.env("ACCESS_TOKEN_SECRET") //config.getString("accessTokenSecret")
 
 }
