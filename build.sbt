5baab7a83842e6d19b5258d7e2b963a286b8aabf
Kate Fulton <katesuzannefulton@gmail.com>
2015-09-19 10:03:39 -0700

    added flyway and postgres and slick

--- build.sbt
+++ build.sbt
@@ -1,5 +1,3 @@
-import NativePackagerKeys._
-
 packageArchetype.java_application
 
 name := "twibot"
@@ -12,5 +10,18 @@ libraryDependencies ++= Seq(
   "org.scalatest" % "scalatest_2.11" % "2.1.7" % "test",
   "org.twitter4j" % "twitter4j-core" % "4.0.3",
   "org.twitter4j" % "twitter4j-stream" % "4.0.3",
-  "com.typesafe"  % "config"          % "1.3.0"
+  "com.typesafe"  % "config"          % "1.3.0",
+  "com.typesafe.slick" %% "slick" % "3.0.3",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "org.postgresql" % "postgresql" % "9.4-1203-jdbc42"
 )
+
+libraryDependencies ++= Seq(
+  "com.h2database" % "h2" % "1.3.174"
+)
+
+Seq(flywaySettings: _*)
+
+flywayUrl := scala.util.Properties.envOrElse("DATABASE_URL", "jdbc:postgresql://localhost:5432/twibot")
+
+flywayUser := "katefulton"
--- src/main/scala/TwitterConfig.scala
+++ src/main/scala/TwitterConfig.scala
@@ -1,8 +1,9 @@
 import com.typesafe.config.Config
 
 class TwitterConfig(config: Config) {
-  val consumerKey = sys.env("CONSUMER_KEY") //config.getString("consumerKey")
-  val consumerSecret = sys.env("CONSUMER_SECRET") //config.getString("consumerSecret")
-  val accessToken = sys.env("ACCESS_TOKEN") //config.getString("accessToken")
-  val accessTokenSecret = sys.env("ACCESS_TOKEN_SECRET") //config.getString("accessTokenSecret")
+  val consumerKey = scala.util.Properties.envOrElse("CONSUMER_KEY", config.getString("consumerKey"))
+  val consumerSecret = scala.util.Properties.envOrElse("CONSUMER_SECRET", config.getString("consumerSecret"))
+  val accessToken = scala.util.Properties.envOrElse("ACCESS_TOKEN", config.getString("accessToken"))
+  val accessTokenSecret = scala.util.Properties.envOrElse("ACCESS_TOKEN_SECRET", config.getString("accessTokenSecret"))
+  val databaseUrl = scala.util.Properties.envOrElse("DATABASE_URL", config.getString("databaseUrl"))
 }
